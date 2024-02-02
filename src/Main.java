f64a229a9839ee2910f8e4d77f9df89278b0ad32
crescimv <crescimv@mail.uc.edu>
2024-02-02 10:25:25 -0500

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        //declarations
+
+        double springCost = 350;
+        double summerCost = 560.78;
+        double fallCost = 423.76;
+        double winterCost = 621.43;
+
+        double totalMaintainance = springCost + summerCost + fallCost + winterCost;
+
+        System.out.println("The total yearly maintanence cost is " + totalMaintainance);
+    }
+}
